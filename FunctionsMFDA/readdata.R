cb0db4d32a3d4e83e45a99694eadd8669055bd27
George Ruck <georgeadruck@gmail.com>
2023-07-14 19:52:52 +0200

    Prepare data for fda form

--- FunctionsMFDA/readdata.R
+++ FunctionsMFDA/readdata.R
@@ -1,2 +1,31 @@
 # Import data
-dfX <- read.csv('.data/xdata_norm.csv')
+setwd("C:/Users/Admin/Documents/R/FDA/FunctionsMFDA")
+dfX <- read.csv("./data/xdata_norm.csv",header = FALSE)
+dfY <- read.csv("./data/ydata_norm.csv",header = FALSE)
+dfZ <- read.csv("./data/zdata_norm.csv",header = FALSE)
+
+# Make functional data objects of x data
+library(fda)
+
+#generate new df for x with each time series in vector form
+ncurves <- ncol(dfX)
+npoints <- nrow(dfX)
+ID <- 1:ncurves
+t <- vector(mode = "list", length = ncurves)
+x <- vector(mode = "list", length = ncurves)
+
+for (i in 1:ncurves){
+  col <- colnames(dfX)[i]
+  t[i] <- list(1:npoints)
+  x[i] <- list(dfX[,col])
+}
+
+columns <- c("id","time","xvalue")
+df <- data.frame(matrix(nrow = ncurves,ncol = length(columns)))
+colnames(df) <- columns
+df$id <- ID
+df$time <- t
+df$xvalue <- x
+
+
+# Use funHDDC for MFDA
